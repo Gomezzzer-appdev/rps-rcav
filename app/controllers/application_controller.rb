710a49c35b572ccb282c6f8824285a435677854a
Jorge Gomez <gomezgeorge132@gmail.com>
2023-03-28 18:36:53 +0000

    Lots of progress on paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end
 
   def play_rock
     # write a TON OF CODE
@@ -12,5 +15,19 @@ class ApplicationController < ActionController::Base
     #render({ :html => "<h1>Howdy, World!</h1>".html_safe })
 
     render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+  @comp_move = ["rock", "paper", "scissors"].sample 
+
+  if @comp_move == "rock" 
+   @outcome =  "won"
+  elsif @comp_move == "paper" 
+    @outcome = "tied"
+  elsif @comp_move == "scissors"  
+    @outcome = "lost"
+  end
+
+    render({ :template => "game_templates/user_paper.html.erb" }) 
   end
 end
